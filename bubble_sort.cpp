4088957d2e82d495239dd4d29f87646b0aed00ea
Samuel Torres <rilla@MacBook-Pro.local>
2025-10-02 14:54:04 -0400

    Finish

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,20 +1,14 @@
-// This program uses a bubble sort to arrange an array of integers in
-// ascending order
-
-// PLACE YOUR NAME HERE
-
 #include <iostream>
 using namespace std;
 
-// function prototypes
 void bubbleSortArray(int[], int);
 void displayArray(int[], int);
 
-const int SIZE = 5;
+const int SIZE = 8;
 
 int main()
 {
-	int values[SIZE] = { 9, 2, 0, 11, 5 };
+	int values[SIZE] = {23, 0, 45, -3, -78, 1, -1, 9};
 
 	cout << "The values before the bubble sort is performed are:" << endl;
 	displayArray(values, SIZE);
@@ -27,59 +21,33 @@ int main()
 	return 0;
 }
 
-//******************************************************************
-//	displayArray
-//
-//  task:	  to print the array
-//  data in:  the array to be printed, the array size
-//  data out: none
-//
-//******************************************************************
-
-void displayArray(int array[], int elems)	// function heading
-{		
-	// displays the array 
+void displayArray(int array[], int elems)
+{
 	for (int count = 0; count < elems; count++)
 		cout << array[count] << "	" << endl;
+	cout << endl;
 }
-
-//******************************************************************
-//	bubbleSortArray
-//
-//  task:	  to sort values of an array in ascending order
-//  data in:  the array, the array size
-//  data out: the sorted array
-//
-//******************************************************************
 
 void bubbleSortArray(int array[], int elems)
 {
 	bool swap;
 	int temp;
-	int bottom = elems - 1;	// bottom indicates the end part of the
-							// array where the largest values have
-							// settled in order 
+	int bottom = elems - 1;
 	do
 	{
 		swap = false;
-
 		for (int count = 0; count < bottom; count++)
 		{
-			if (array[count] > array[count + 1])
-			{		
-				// the next three lines do a swap 
+			if (array[count] < array[count + 1])
+			{
 				temp = array[count];
 				array[count] = array[count + 1];
 				array[count + 1] = temp;
 
-				swap = true; // indicates that a swap occurred
+				swap = true;
 			}
+			displayArray(array, elems);
 		}
-
-		bottom--;	// bottom is decremented by 1 since each pass through
-					// the array adds one more value that is set in order
-
+		bottom--;
 	} while (swap != false);
-	// loop repeats until a pass through the array with
-	// no swaps occurs
 }
